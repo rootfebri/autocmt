ee00b9124f726ff007a35a3c0aae429f8221e877
Febri <root.febri@gmail.com>
2024-09-13 21:50:16 +0700

    delete-profile.ts done

--- src/actions/delete-profile.ts
+++ src/actions/delete-profile.ts
@@ -3,24 +3,21 @@ import fs from "fs";
 import Profile from "../../models/profile";
 import {fmtProfiles, inqTheme} from "../helpers/lib";
 import chalk from "chalk";
-import prompts from "prompts";
 
 export default async function () {
     const instructions = chalk.dim(' (↑ ↓ untuk bergerak [tekan :ENTER: jika panah stuck], :SPASI: untuk memilih, :ENTER: Hapus profile, `a` Pilih Semua, `i` Membalik Pilihan)');
     try {
-        const choices = await Profile.findAll().then(profiles => profiles.map(profile => ({title: profile.name, value: profile})));
-
-        const profiles = await prompts({
+        const choices = await Profile.findAll().then(fmtProfiles);
+        const profiles = await checkbox({
             message: 'Pilih profil untuk dihapus',
+            required: true,
             choices,
-            type: 'multiselect',
-            name: 'data',
-            hint: instructions,
+            instructions,
+            theme: inqTheme
         });
 
-        for (const profile of profiles.data) {
-            console.log(typeof profile, profile)
-            // await deleteRecursive(profile)
+        for (const profile of profiles) {
+            await deleteRecursive(profile)
         }
     } catch (e: any) {
         console.error('Gagal menghapus profile', e.message || e)
